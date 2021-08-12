714cb0b9dd766abddc2db3ed0d8fcf0e6d43904b
kuked <kedoku@gmail.com>
2021-08-12 17:19:10 +0900

    Add a basic test for boolean operation

--- test/lr_test.rb
+++ test/lr_test.rb
@@ -11,5 +11,14 @@ class LrTest < Minitest::Test
     assert_output("0.0\n") { vm.interpret("print 1 + 2 - 3;") }
     assert_output("-9.0\n") { vm.interpret("print 1 + 2 - 3 * 4;") }
     assert_output("0.6000000000000001\n") { vm.interpret("print 1 + 2 - 3 * 4 / 5;") } # yuck :-<
+    assert_output("9.0\n") { vm.interpret("print (1 + 2) * 3;") }
+  end
+
+  def test_boolean_operation
+    vm = Lr::VM.new
+    assert_output("true\n") { vm.interpret("print true;") }
+    assert_output("false\n") { vm.interpret("print false;") }
+    assert_output("true\n") { vm.interpret("print !false;") }
+    assert_output("false\n") { vm.interpret("print !true;") }
   end
 end
